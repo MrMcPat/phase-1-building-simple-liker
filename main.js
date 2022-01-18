2ec3a5544fac8c4e6596b9a36cdbda08eef5182d
Patrick Liang <pl204511194@yahoo.com>
2022-01-18 10:29:43 -0500

    commit tree 448986d68a84d0c9b3a8b69cda62ca16fc7bd313

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+const errorModal = document.getElementById("modal");
 
 // Your JavaScript code goes here!
+document.getElementById("modal").classList.add("hidden");
+const allLikes = document.querySelectorAll(".like-glyph");
+allLikes.forEach(like => {
+  like.addEventListener("click", event => {
+    console.log(event.target.innerHTML);
+    mimicServerCall()
+    .then(data => {
+      console.log(data);
+      if (event.target.innerHTML === EMPTY_HEART) {
+        event.target.innerText = FULL_HEART;
+        event.target.classList.add("activated-heart");
+      } else {
+        event.target.innerText = EMPTY_HEART;
+        event.target.classList.remove("activated-heart");
+      };
+    }) 
 
+    .catch((event) => {
+      console.log(event);
+      errorModal.classList.remove("hidden");
+      errorModal.innerText = event;
+      setTimeout(() => errorModal.classList.add("hidden"), 3000);
+    });
 
+  })
+});
 
 
 //------------------------------------------------------------------------------
